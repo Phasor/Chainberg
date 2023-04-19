004fbffcb10a1542f094a65abbb7ec8d20f3df21
ben <phas0ruk@gmail.com>
2023-04-19 16:08:04 -0700

    improves prompt

--- pages/api/query-csv.js
+++ pages/api/query-csv.js
@@ -8,10 +8,21 @@ export default async function handler(req, res) {
     const prompt = `You are an AI model that translates natural language queries about United States government treasury bond yields into MongoDB queries. Given a question, you will provide the corresponding MongoDB query.
 
     Question: How many times has the 2-year treasury yield been above the 10-year treasury yield? This is called yield curve inversion.
-    MongoDB Query: { "$expr": { "$gt": ["$two_year_yield", "$ten_year_yield"] } }
+    MongoDB Query: [
+        { "$match": { "$expr": { "$gt": ["two_year_yield", "ten_year_yield"] } } }
+    ]
     
     Question: How many times has the 2-year treasury yield been below the 10-year treasury yield?
-    MongoDB Query: { "$expr": { "$lt": ["$two_year_yield", "$ten_year_yield"] } }
+    MongoDB Query: [
+        { "$match": { "$expr": { "$lt": ["two_year_yield", "ten_year_yield"] } } }
+    ]
+    
+    Question: Has the 2-year treasury yield ever been higher than the 10-year treasury yield? If so, when?
+    MongoDB Query: [
+        { "$match": { "$expr": { "$gt": ["two_year_yield", "ten_year_yield"] } } },
+        { "$sort": { "date": -1 } },
+        { "$limit": 1 }
+    ]
     
     Question: What is the highest yield the 2-year treasury yield has reached over the last 1 year?
     MongoDB Query:
@@ -37,13 +48,16 @@ export default async function handler(req, res) {
         ]
     
     Use the above to produce a MongoDB query for the following question, ensuring you only reply with the MongoDB query, 
-    no other words or numbers that are not part of the query. Do not include the word "MongoDB Query:" in your response.
+    no other words or numbers that are not part of the query. Do not include the word "MongoDB Query:" in your response. Give the dates in your reply where appropriate.
     
     ${userQuestion}`;
 
     const response = await model.generate([prompt]);
-    const mongoQuery = response.generations[0][0].text.replace('MongoDB Query: ', '');
-    console.log(`MongoDB query is ${mongoQuery}`);
+    const mongoQuery = response.generations[0][0].text.replace('MongoDB Query: ', '').trim();
+    // Remove trailing commas from arrays in JSON strings
+    const cleanedMongoQuery = mongoQuery.replace(/\,\s*\]/g, ']');
+
+    console.log(`MongoDB query is ${cleanedMongoQuery}`);
     const uri = process.env.MONGO_URI;
     
     // Connect to the MongoDB database
@@ -52,14 +66,14 @@ export default async function handler(req, res) {
     // Get the collection to run the query against
     const collection = client.db().collection('treasurydatas');
 
-    // Execute the query
-    let pipeline = [];
+    let pipeline;
     try {
-      pipeline = JSON.parse(mongoQuery);
+        pipeline = JSON.parse(cleanedMongoQuery);
     } catch (e) {
-      console.error(`Error parsing MongoDB query: ${e}`);
-      return res.status(500).json({ message: 'Internal Server Error' });
+        console.error(`Error parsing MongoDB query: ${e}`);
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
+
     const result = await collection.aggregate(pipeline).toArray();
 
     // Close the MongoDB connection
